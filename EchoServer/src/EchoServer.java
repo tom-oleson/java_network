2cf64bad51837c225cd90e8f842c9acf53159909
tom.oleson <tom.oleson@gmail.com>
2020-03-03 17:29:04 -0500

    Add formatHexDump and formatHexRecord

--- EchoServer/src/EchoServer.java
+++ EchoServer/src/EchoServer.java
@@ -22,35 +22,129 @@ public class EchoServer {
 		System.err.println("error: "+s);
 	}
 
-	public static String bytesToHex(byte[] bytes, int sz, int width) {
-		StringBuilder out = new StringBuilder();
-		StringBuilder hex = new StringBuilder();
-		StringBuilder ascii = new StringBuilder();
-		for(int index = 0; index < sz; index++) {
+	public static String formatHexRecord(byte[] bytes, int offset, int sz) {
+		StringBuilder builder = new StringBuilder();
+
+		for(int index = offset; index < sz; index++) {
 			int value = bytes[index] & 0x00FF;
-			String str = Integer.toHexString(value);
-			if (str.length() == 1)
-				hex.append('0');
-			hex.append(str);
-			hex.append(' ');
-
-			if(value < 0x20 || value > 0x7e) {
-				ascii.append('.');
-			} else {
-				ascii.append( (char) value);
-			}
-			
-			if(index > 0 && index % width == 0) {
-				out.append(hex);
-				out.append("| ");
-				out.append(ascii);
-				out.append("\n");
-
-				hex.setLength(0);
-				ascii.setLength(0);
-			}
+			builder.append(String.format("%02x", value));
 		}
-		return out.toString();
+		// line seperator
+		builder.append(String.format("%n"));
+		return builder.toString();
+	}
+
+	public static String formatHexDump(byte[] bytes, int offset, int sz, int width) {
+
+		StringBuilder builder = new StringBuilder();
+
+		for(int row_offset = offset; row_offset < offset + sz; row_offset += width) {
+
+			builder.append(String.format("%06d: ", row_offset));
+
+			StringBuilder ascii = new StringBuilder();
+
+			// row of hex digits of specified width followed by ascii field...
+			// non-printable characters are output as '.'
+			for(int index = 0; index < width; index++) {
+				if(row_offset + index < sz) {
+					int value = bytes[row_offset + index] & 0x00FF;
+					builder.append(String.format("%02x ", value));
+
+					if(value < 0x20 || value > 0x7e) {
+						ascii.append('.');
+					} else {
+						ascii.append( (char) value);
+					}
+
+				} else {
+					builder.append("   ");
+				}
+			}
+
+			if(row_offset < sz) {
+				builder.append(" | ");
+				builder.append(ascii);
+			}
+
+			// line seperator
+			builder.append(String.format("%n"));
+
+		}
+		return builder.toString();
+	}
+	
+	private static class EchoTask implements Callable<Void> {
+
+		private Socket connection;
+	
+		EchoTask(Socket connection) {
+			this.connection = connection;
+		}
+
+		public int readBytes(BufferedInputStream bis, byte[] buf) throws IOException {
+
+			// read until we get data or an error...
+			int num_bytes = 0;
+			while(num_bytes == 0) {
+				num_bytes = bis.read(buf);
+				if(num_bytes == -1) return -1;
+			} 
+			return num_bytes;
+		}
+
+		public void processBytes(BufferedOutputStream bos, byte[] buf, int sz) throws IOException {
+
+			// output to server console
+			System.out.println("======");
+			System.out.print(formatHexDump(buf, 0, sz, 16));
+			System.out.print(formatHexRecord(buf, 0, sz));
+
+			// echo to client stream
+			bos.write(buf);
+			bos.flush();
+		}
+
+		@Override
+		public Void call() {
+
+			info("Running thread for "+connection.toString()+" [connection timeout="+SOCKET_TIMEOUT+"]");
+			try {
+				BufferedInputStream bis = new BufferedInputStream(connection.getInputStream());
+				BufferedOutputStream bos = new BufferedOutputStream(connection.getOutputStream());
+
+				String hello = "EchoServerHello\r\n";
+				bos.write(hello.getBytes());
+				bos.flush();
+
+				int read_count = 0;
+				byte[] buf = new byte[4096];
+
+				while(true) {
+					if((read_count = readBytes(bis, buf)) > 0) {				
+						processBytes(bos, buf, read_count);
+
+						// clear the buffer
+						Arrays.fill(buf, (byte) 0);
+					}
+					// EOF on client disconnect
+					else if(read_count < 0) {
+						info("***EOF***: "+connection.toString());
+						break;
+					}
+				}
+			}
+			catch (IOException ex) { err(ex.getMessage()); }
+			finally { 
+				try {
+					connection.close();
+					info("connection closed: "+connection.toString());
+				}
+				catch (IOException ex) { err("close failed: "+ex.getMessage()); } 
+			}
+
+			return null;
+		} 
 	}
 
 	public static void main(String[] args) {
@@ -83,80 +177,4 @@ public class EchoServer {
 			err("could not start server: "+ex.getMessage());
 		}
 	} 
-	
-	private static class EchoTask implements Callable<Void> {
-
-		private Socket connection;
-	
-		EchoTask(Socket connection) {
-			this.connection = connection;
-		}
-
-
-		public int readBytes(BufferedInputStream bis, byte[] buf) throws IOException {
-
-			// read until we get data or an error...
-			int num_bytes = 0;
-			while(num_bytes == 0) {
-				num_bytes = bis.read(buf);
-				if(num_bytes == -1) return -1;
-			} 
-			return num_bytes;
-		}
-
-		public void processBytes(BufferedOutputStream bos, byte[] buf, int sz) throws IOException {
-
-			// output to server console
-			System.out.print(bytesToHex(buf, sz, 16));
-			System.out.write(buf);
-
-			// echo to client stream
-			bos.write(buf);
-			bos.flush();
-		}
-
-
-		@Override
-		public Void call() {
-
-			info("Running thread for "+connection.toString()+" [connection timeout="+SOCKET_TIMEOUT+"]");
-			try {
-				BufferedInputStream bis = new BufferedInputStream(connection.getInputStream());
-				BufferedOutputStream bos = new BufferedOutputStream(connection.getOutputStream());
-
-				String hello = "EchoServerHello\r\n";
-				bos.write(hello.getBytes());
-				bos.flush();
-
-				int read_count = 0;
-				byte[] buf = new byte[2048];
-
-				do {
-					if((read_count = readBytes(bis, buf)) > 0) {				
-						processBytes(bos, buf, read_count);
-
-						// clear the buffer
-						Arrays.fill(buf, (byte) 0);
-					}
-					// EOF on client disconnect
-					else if(read_count < 0) {
-						info("***EOF***: "+connection.toString());
-						break;
-					}
-
-				} while(true);
-			
-			}
-			catch (IOException ex) { err(ex.getMessage()); }
-			finally { 
-				try {
-					connection.close();
-					info("connection closed: "+connection.toString());
-				}
-				catch (IOException ex) { err("close failed: "+ex.getMessage()); } 
-			}
-
-			return null;
-		} 
-	}
 } 
