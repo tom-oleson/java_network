daa27c82ce22ee0bc7c5b1e9c4e3e17be325a766
Tom Oleson <tom.oleson@gmail.com>
2020-03-27 15:08:51 -0400

    Add echo host and port

--- RouteServer/src/test/sendall.sh
+++ RouteServer/src/test/sendall.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+HOST=localhost
+PORT=4000
+
+echo host=$HOST, port=$PORT
+
 for f in request1 request2 request3 request4 request5
 do
         echo $f Request
 	./hex2bin.sh $f > bin_out
 	cat bin_out | hexdump -C
 	echo $f Response
-	cat bin_out | nc -N localhost 4000 | hexdump -C 
+	cat bin_out | nc -N $HOST $PORT | hexdump -C 
 done
 
