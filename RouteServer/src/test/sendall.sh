d0912f43f871ec72e8e86d68ed2f8ca807732bf2
Tom Oleson <tom.oleson@gmail.com>
2020-03-28 10:41:38 -0400

    Output to /dev/shm (ramdisk)

--- RouteServer/src/test/sendall.sh
+++ RouteServer/src/test/sendall.sh
@@ -9,8 +9,8 @@ for f in request1 request2 request3 request4 request5
 do
         echo $f Request
 	./hex2bin.sh $f > bin_out
-	cat bin_out | hexdump -C
+	cat /dev/shm/bin_out | hexdump -C
 	echo $f Response
-	cat bin_out | nc -N $HOST $PORT | hexdump -C 
+	cat /dev/shm/bin_out | nc -N $HOST $PORT | hexdump -C 
 done
 
