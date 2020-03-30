ded8625ea57300a7b3e7b02ddd9581b1f9703086
Tom Oleson <tom.oleson@gmail.com>
2020-03-30 14:02:11 -0400

    Also dump hex strings for request/response

--- RouteServer/src/test/sendone.sh
+++ RouteServer/src/test/sendone.sh
@@ -2,13 +2,19 @@
 
 HOST=localhost
 PORT=4000
+BIN_FILE=/dev/shm/bin_out
+BIN_RESPONSE=/dev/shm/bin_resp
 
 echo host=$HOST, port=$PORT
 
 file=$1
-echo $file Request
-./hex2bin.sh $file > /dev/shm/bin_out
-cat /dev/shm/bin_out | hexdump -C
-echo $file Response
-cat /dev/shm/bin_out | nc -N $HOST $PORT | hexdump -C 
+echo ====
+echo $file Request:
+cat $file
+./hex2bin.sh $file > $BIN_FILE
+cat $BIN_FILE | hexdump -C
+echo ----
+echo $file Response:
+cat $BIN_FILE | nc -w30 $HOST $PORT > $BIN_RESPONSE && ./bin2hex.sh $BIN_RESPONSE && echo && hexdump -C $BIN_RESPONSE
+echo ====
 
